7b6ae93a30e1dbb0569892c57f83795348a96c8a
ElBigas <elbigas@protonmail.com>
2023-10-04 10:25:30 -0300

    Criando a interface Autenticavel para implementar nos perfils que poderão ter acesso ao sistema ficticio

--- autenticacao.php
+++ autenticacao.php
@@ -1,14 +1,20 @@
 <?php
 
+use Alura\Banco\Modelo\Conta\Titular;
 use Alura\Banco\Modelo\Cpf;
+use Alura\Banco\Modelo\Endereco;
 use Alura\Banco\Modelo\Funcionario\Diretor;
 use Alura\Banco\Service\Autenticador;
 
 require_once 'autoload.php';
 
-$autenticador = new Autenticador();
 $cpf = new Cpf('123.456.789-10');
+$endereco = new Endereco('Londrina', 'Centro', 'JK', '1234');
+$titular = new Titular('Emanuel', $cpf, $endereco);
+$funcionario = new Diretor('Maria', $cpf, 4000);
 
-$diretor = new Diretor('Emanuel', $cpf, 4000);
+$autenticador = new Autenticador();
 
-$autenticador->tentaLogin($diretor, '1234');
+$autenticador->tentaLogin($titular, 'abcd');
+echo PHP_EOL;
+$autenticador->tentaLogin($funcionario, '1234');
